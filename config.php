df44c4a0c78d1461856f380794477ec07eb33b37
Your Nardone <nardonedavid@hotmail.com>
2023-02-08 14:48:03 +0100

    first commit

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+const DB_HOST = 'localhost';
+const DB_NAME = 'subscribers01';
+const DB_USER = 'root';
+const DB_PASSWORD= '';
+
+// define( 'DB_HOST', 'localhost');
+// define('DB_NAME', 'subscribers01');
+// define('DB_USER', 'root');
+// define('DB_PASSWORD', 'password');   
+        
+
